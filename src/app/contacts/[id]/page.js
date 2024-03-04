858b7aec749a7914fc7fef8b177aa6df4e7ced47
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-03-04 23:13:20 +0600

    chore: update contact forms

--- src/app/contacts/[id]/page.js
+++ src/app/contacts/[id]/page.js
@@ -1,14 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import SaveContactButton from "@/components/save";
-import { createContact, isUserAuthenticated } from "../../../../server/actions";
-import { redirect } from "next/navigation";
+import { getContact, updateContact } from "../../../../server/actions";
 
-export default async function CreateContactPage() {
-    const authenticated = await isUserAuthenticated();
+export default function ContactEditPage({ params }) {
+    const router = useRouter();
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        phoneNumber: "",
+        address: "",
+    });
 
-    if (!authenticated) {
-        redirect("/api/auth/login");
-    }
+    useEffect(() => {
+        getContact(params.id).then(response => {
+            if (response.success) {
+                setData(response.data);
+            }
+        });
+    }, [params.id]);
+
+    const handleInputChange = event => {
+        let value = event.target.value;
+        setData({ ...data, [event.target.name]: value });
+    };
 
     return (
         <section className="max-w-4xl p-6 my-12 mx-auto bg-white rounded-md shadow-md">
@@ -16,10 +33,9 @@ export default async function CreateContactPage() {
 
             <form
                 action={async formData => {
-                    "use server";
-                    const response = await createContact(formData);
+                    const response = await updateContact(data.id, formData);
                     if (response.success) {
-                        redirect("/contacts");
+                        router.push("/contacts");
                     } else {
                         throw new Error(response.message);
                     }
@@ -34,6 +50,8 @@ export default async function CreateContactPage() {
                             id="name"
                             name="name"
                             type="text"
+                            value={data.name}
+                            onInput={handleInputChange}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                         />
                     </div>
@@ -46,6 +64,8 @@ export default async function CreateContactPage() {
                             id="email"
                             name="email"
                             type="email"
+                            value={data.email}
+                            onInput={handleInputChange}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                         />
                     </div>
@@ -58,6 +78,8 @@ export default async function CreateContactPage() {
                             id="phoneNumber"
                             name="phoneNumber"
                             type="text"
+                            value={data.phoneNumber}
+                            onInput={handleInputChange}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                         />
                     </div>
@@ -70,12 +92,14 @@ export default async function CreateContactPage() {
                             id="address"
                             name="address"
                             type="text"
+                            value={data.address}
+                            onInput={handleInputChange}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
                         />
                     </div>
                 </div>
 
-                <SaveContactButton label="Save Contact" loading="Saving..." />
+                <SaveContactButton label="Update Contact" loading="Updating..." />
             </form>
         </section>
     );
--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,47 +1,20 @@
 import React from "react";
 import { redirect } from "next/navigation";
-import { isUserAuthenticated, getAllContactsByUser } from "@/../server/actions";
 import Image from "next/image";
+import Link from "next/link";
+
+import { isUserAuthenticated, getAllContactsByUser, deleteContact } from "@/../server/actions";
+import ContactDeleteIconButton from "@/components/delete";
+import ContactEditIconButton from "@/components/edit";
 
 export default async function ContactsPage() {
     const authenticated = await isUserAuthenticated();
-
-    const contacts = [
-        {
-            id: 1,
-            name: "Mehedi",
-            address: "Dhaka, Bangladesh",
-            email: "mehedi@gmail.com",
-            phoneNumber: "+880171XXXXXXX",
-        },
-        {
-            id: 2,
-            name: "Sakib",
-            address: "Chittagong, Bangladesh",
-            email: "sakib@gmail.com",
-            phoneNumber: "+880171YYYYYYY",
-        },
-        {
-            id: 3,
-            name: "Rakib",
-            address: "Sylhet, Bangladesh",
-            email: "rakib@gmail.com",
-            phoneNumber: " +880171ZZZZZZZ",
-        },
-        {
-            id: 4,
-            name: "Ashraf",
-            address: "Sylhet, Bangladesh",
-            email: "ashraf@gmail.com",
-            phoneNumber: " +880171SSSSSSSS",
-        },
-    ];
 
     if (!authenticated) {
         redirect("/api/auth/login");
     }
 
-    const data = await getAllContactsByUser();
+    const contacts = await getAllContactsByUser();
 
     return (
         <section className="container px-4 mx-auto my-12">
@@ -50,7 +23,7 @@ export default async function ContactsPage() {
                     <div className="flex items-center gap-x-3">
                         <h2 className="text-lg font-medium text-gray-800">Contacts</h2>
 
-                        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full">240 contacts</span>
+                        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full">{contacts?.data.length} contacts</span>
                     </div>
 
                     <p className="mt-1 text-sm text-gray-500">Contacts are sorted alphabetically</p>
@@ -69,7 +42,9 @@ export default async function ContactsPage() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
 
-                        <span>Add Contact</span>
+                        <span>
+                            <Link href="/contacts/create">Add Contact</Link>
+                        </span>
                     </button>
                 </div>
             </div>
@@ -129,7 +104,7 @@ export default async function ContactsPage() {
                                     </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                    {contacts?.map(contact => {
+                                    {contacts?.data.map(contact => {
                                         return (
                                             <tr key={contact.id}>
                                                 <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
@@ -158,39 +133,10 @@ export default async function ContactsPage() {
                                                 <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{contact.address}</td>
                                                 <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                     <div className="flex items-center gap-x-6">
-                                                        <button className="text-gray-500 transition-colors duration-200 hover:text-red-500 focus:outline-none">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none"
-                                                                viewBox="0 0 24 24"
-                                                                strokeWidth="1.5"
-                                                                stroke="currentColor"
-                                                                className="w-5 h-5"
-                                                            >
-                                                                <path
-                                                                    strokeLinecap="round"
-                                                                    strokeLinejoin="round"
-                                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                                                />
-                                                            </svg>
-                                                        </button>
-
-                                                        <button className="text-gray-500 transition-colors duration-200 hover:text-yellow-500 focus:outline-none">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none"
-                                                                viewBox="0 0 24 24"
-                                                                strokeWidth="1.5"
-                                                                stroke="currentColor"
-                                                                className="w-5 h-5"
-                                                            >
-                                                                <path
-                                                                    strokeLinecap="round"
-                                                                    strokeLinejoin="round"
-                                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                                                                />
-                                                            </svg>
-                                                        </button>
+                                                        <ContactDeleteIconButton id={contact.id} />
+                                                        <Link href={`/contacts/${contact.id}`}>
+                                                            <ContactEditIconButton />
+                                                        </Link>
                                                     </div>
                                                 </td>
                                             </tr>
