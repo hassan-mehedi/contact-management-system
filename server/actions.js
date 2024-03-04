7f89198997f3d32ea1a6063a1fd790288a0b8a49
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-03-04 23:12:35 +0600

    chore: add get, update and delete contact actions

--- server/actions.js
+++ server/actions.js
@@ -3,6 +3,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 async function isUserAuthenticated() {
     const { isAuthenticated } = getKindeServerSession();
@@ -29,18 +30,26 @@ async function getAllContactsByUser() {
     }
 }
 
-async function createContact(formState, formData) {
+async function createContact(formData) {
     try {
         const name = formData.get("name");
         const email = formData.get("email");
         const phoneNumber = formData.get("phoneNumber");
         const address = formData.get("address");
 
+        if (!name) {
+            return { success: false, message: "Please fill the name field" };
+        }
+
+        if (!email && !phoneNumber && !address) {
+            return { success: false, message: "Please fill at least one type of contact" };
+        }
+
         const user = await getUserDetails();
         const userEmail = user.email;
 
         const prisma = new PrismaClient();
-        const data = await prisma.contact.create({
+        await prisma.contact.create({
             data: {
                 name,
                 email,
@@ -50,15 +59,88 @@ async function createContact(formState, formData) {
             },
         });
 
-        console.log(data);
-
         revalidatePath("/contacts");
 
-        return { message: "Contact created" };
+        return { success: true, message: "Successfully created the contact" };
     } catch (error) {
         console.error(error);
-        return { message: "Something went wrong" };
     }
 }
 
-export { isUserAuthenticated, getAllContactsByUser, getUserDetails, createContact };
+async function getContact(contactId) {
+    try {
+        const prisma = new PrismaClient();
+        const data = await prisma.contact.findUnique({
+            where: {
+                id: contactId,
+            },
+        });
+
+        if (!data) {
+            return { success: false, error: false, message: "No contact found" };
+        }
+
+        return { success: true, message: "Successfully found the contact", data: data };
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function updateContact(contactId, formData) {
+    try {
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const phoneNumber = formData.get("phoneNumber");
+        const address = formData.get("address");
+
+        if (!name) {
+            return { success: false, message: "Please fill the name field" };
+        }
+
+        if (!email && !phoneNumber && !address) {
+            return { success: false, message: "Please fill at least one type of contact" };
+        }
+
+        const user = await getUserDetails();
+        const userEmail = user.email;
+
+        const prisma = new PrismaClient();
+        await prisma.contact.update({
+            where: {
+                id: contactId,
+            },
+            data: {
+                name,
+                email,
+                address,
+                phoneNumber,
+                userEmail,
+            },
+        });
+
+        revalidatePath("/contacts");
+
+        return { success: true, message: "Successfully updated the contact" };
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function deleteContact(contactId) {
+    try {
+        const prisma = new PrismaClient();
+        await prisma.contact.delete({
+            where: {
+                id: contactId,
+            },
+        });
+
+        revalidatePath("/contacts");
+
+        return { success: true, message: "Successfully deleted the contact" };
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export { isUserAuthenticated, getAllContactsByUser, getUserDetails, createContact, getContact, updateContact, deleteContact };
